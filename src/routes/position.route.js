c83fbd09bed830d1ce5614df28e2e246cef53539
Shariful Islam <rocky.cse09@gmail.com>
2022-02-26 20:32:14 +0600

    Position creation joi validation done

--- src/routes/position.route.js
+++ src/routes/position.route.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// const positionValidation = require('./../validations/position.validation');
+const validate = require('./../middlewares/validate');
+const positionValidation = require('./../validations/position.validation');
 const positionController = require('./../controllers/position.controller');
 
 router
     .route('/')
-    .post(positionController.createPosition)
+    .post(
+        validate(positionValidation.createPosition),
+        positionController.createPosition
+    )
     .get(positionController.getPositions);
 
 module.exports = router;
